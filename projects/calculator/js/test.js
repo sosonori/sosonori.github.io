8334a0f5cd5d630e8c7f7c6cd4b2327f8ade43bf
foryun3 <foryun3@gmail.com>
2022-01-18 17:39:13 +0900

    test

--- projects/calculator/js/test.js
+++ projects/calculator/js/test.js
@@ -1,18 +1,29 @@
+const defaultPath = '/projects/calculator';
+let afterPath = window.location.pathname;
 window.onload = () => {
     const historyLinker = document.querySelectorAll('.tab_history');
+    window.history.replaceState(null, '', window.location.origin + '/projects/calculator')
 
     historyLinker.forEach((element) => {
         element.addEventListener('click', (event) =>{
             const dataset = event.target.dataset;
             const contentDiv = document.getElementById('pageCont');
-            historyRouerPush(dataset, contentDiv);
+
+            const type = (beforePath == defaultPath) ? 'push' : 'replace';
+            historyRouerPush(dataset, type);
         })
     });
 }
 
 const historyRouerPush = (dataset, element) => {
     const state = { id: dataset.id, route: dataset.route };
-    window.history.pushState(state, state.route, window.location.origin + state.route);
+
+    if(type == 'push') {
+        window.history.pushState(state, state.route, window.location.origin + state.route);
+    } else {
+        window.history.replaceState(state, state.route, window.location.origin + state.route);
+    }
+    
     renderHTML(element, dataset.route);
 }
 
@@ -20,4 +31,6 @@ const renderHTML = (element, route) => {
     element.innerHTML = route;
 }
 
-window.onpopstate = () => renderHTML(element, window.location.pathname);
+window.onpopstate = (history) => {
+    if(history.state == null || history.state.route == afterPath)
+    renderHTML(element, window.location.pathname);}
